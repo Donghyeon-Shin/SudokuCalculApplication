828a4466cb68f3b4deebac29fb57d378b5e2dee4
Donghyeon Shin <shindong0321@gmail.com>
2022-12-15 23:29:11 +0900

    ENH:Reset Button 및 BT 알고리즘으로 Calculation 함수 구현

--- Assets/Scripts/Sudoku.cs
+++ Assets/Scripts/Sudoku.cs
@@ -1,31 +1,33 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Sudoku : MonoBehaviour
 {
-    public GameObject InputButton, NumberButton, EmptyButton;
+    public GameObject InputButton, NumberButton, EmptyButton, ResetButton;
     public Image image;
     private Animator animator;
 
     int[,] arr = new int[9, 9]; // Sudoku array
+    bool ResetCheck = false;
 
     private void Update()
     {
+        // Empty Button Activation
         if (InputButton != null)
         {
             string value = InputButton.GetComponentInChildren<Text>().text;
-            if (value != "")
-            {
-                EmptyButton.SetActive(true);
-            }
-            else
-            {
-                EmptyButton.SetActive(false);
-            }
+            if (value != "") EmptyButton.SetActive(true);
+            else EmptyButton.SetActive(false);
         }
-        else EmptyButton.SetActive(false);
+        else
+            EmptyButton.SetActive(false);
+
+        // Reset Button Activation
+        if (ResetCheck) ResetButton.SetActive(true);
+        else ResetButton.SetActive(false);
     }
 
     public void ClickEmptyButton()
@@ -39,6 +41,20 @@ public class Sudoku : MonoBehaviour
             int c = name[3] - '0';
             // Set array[column][row] 
             arr[c, r] = 0;
+
+            // Verify Number Table Has number
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (arr[i, j] != 0)
+                    {
+                        ResetCheck = true;
+                        return;
+                    }
+                }
+            }
+            ResetCheck = false;
         }
     }
 
@@ -76,19 +92,146 @@ public class Sudoku : MonoBehaviour
             int c = name[3] - '0';
             // Set array[column][row] 
             arr[c, r] = n[0] - '0';
+            ResetCheck = true;
         }
     }
 
-    // Start Calculation Button
-    public void StartCalculation()
+    // Click Reset Button
+    public void ClickResetButton()
     {
-        // Test Print out Sudoku array
+        ResetTableAndArray();
+    }
+
+    // Start Calculation Button
+    public void ClickStartCalculationButton()
+    {
+        bool check = calcultaion();
+        if (check)
+        {
+            // Test Print out Sudoku array
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Debug.Log(arr[i, j]);
+                }
+            }
+        }
+        // Reset number Table
+        ResetTableAndArray();
+    }
+
+    // temporary button
+    private GameObject Button;
+
+    private void ResetTableAndArray()
+    {
+        // Check if the Curr Table Number Button is enabled
+        if (InputButton != null)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    // Initialize to zero
+                    string prefix = "RC";
+                    string name = prefix + i + j;
+                    Button = GameObject.Find(name);
+                    Button.GetComponentInChildren<Text>().text = "";
+                    arr[i, j] = 0;
+                }
+            }
+            // Unassign Curr Button
+            ResetCheck = false;
+            image = InputButton.GetComponent<Image>();
+            image.color = Color.black;
+            InputButton = null;
+        }
+    }
+
+    /* Sudoku Calculation */
+    List<Pos> blank = new List<Pos>();
+    int cnt = 0; // blank count
+    bool flag; // flag = Calculation Complete
+
+    // Coordinate structure
+    struct Pos
+    {
+        public int x;
+        public int y;
+
+        public Pos(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private bool calcultaion()
+    {
+        // Initialize
+        blank.Clear();
+        cnt = 0;
+        flag = false;
+
+        // Check blank(0)
         for (int i = 0; i < 9; i++)
         {
             for (int j = 0; j < 9; j++)
             {
-                Debug.Log(arr[i, j]);
+                if (arr[i, j] == 0)
+                {
+                    cnt++;
+                    blank.Add(new Pos(i, j));
+                }
             }
         }
+
+        // Backtracking
+        if (cnt > 0) bt(0);
+        return true;
+    }
+
+    // Backtracking function
+    private void bt(int n)
+    {
+        if (cnt == n)
+        {
+            flag = true;
+            return;
+        }
+
+        int x = blank[n].x;
+        int y = blank[n].y;
+        for (int i = 1; i <= 9; i++)
+        {
+            arr[x, y] = i;
+            // Check the number entered is correct
+            if (check(x, y)) bt(n + 1);
+            if (flag) return;
+        }
+        arr[x, y] = 0;
+        return;
+    }
+
+    // Check row, column, 3*3 
+    private bool check(int x, int y)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (arr[i, y] == arr[x, y] && i != x) return false;
+            if (arr[x, i] == arr[x, y] && i != y) return false;
+        }
+        int vert_index = (x / 3) * 3;
+        int horiz_index = (y / 3) * 3;
+
+        for (int i = vert_index; i < vert_index + 3; i++)
+        {
+            for (int j = horiz_index; j < horiz_index + 3; j++)
+            {
+                if (arr[x, y] == arr[i, j] && x != i && y != j) return false;
+            }
+        }
+        return true;
     }
 }
